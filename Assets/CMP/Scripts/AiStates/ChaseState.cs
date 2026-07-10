f471d51523b0addccc64786d72eaefd8010ccf45
huseyinbaglayici <huseyinow35@hotmail.com>
2026-07-11 00:54:22 +0300

    fix: lock chase direction until the next corner

--- Assets/CMP/Scripts/AiStates/ChaseState.cs
+++ Assets/CMP/Scripts/AiStates/ChaseState.cs
@@ -21,16 +21,42 @@ namespace CMP.Scripts.AiStates
             if (GhostBlackboard.Ghost.IsMoving)
                 return;
 
-            Vector2Int start = GhostBlackboard.CurrentGridPos;
-            Vector2Int goal = GhostBlackboard.GameManager.Pacman.CurrentGridPos;
-            var path = Pathfinding.FindPath(start, goal, _isWalkable);
+            Vector2Int pos = GhostBlackboard.CurrentGridPos;
+            Direction heading = GhostBlackboard.Heading;
 
+            if (!IsAtCorner(pos, heading))
+            {
+                Vector2Int forward = pos + heading.ToVector2Int();
+                if (GhostBlackboard.GridData.IsMovable(forward))
+                {
+                    GhostBlackboard.Ghost.MoveTo(forward);
+                    return;
+                }
+            }
+
+            Vector2Int goal = GhostBlackboard.GameManager.Pacman.CurrentGridPos;
+            var path = Pathfinding.FindPath(pos, goal, _isWalkable);
             if (path == null || path.Count == 0)
                 return;
 
             Vector2Int next = path[0];
-            GhostBlackboard.Heading = (next - start).ToDirection();
+            GhostBlackboard.Heading = (next - pos).ToDirection();
             GhostBlackboard.Ghost.MoveTo(next);
+        }
+
+        private bool IsAtCorner(Vector2Int pos, Direction heading)
+        {
+            Direction reverse = heading.Reverse();
+            foreach (var dir in GameSettings.DirectionsToCheck)
+            {
+                if (dir == heading || dir == reverse)
+                    continue;
+
+                if (GhostBlackboard.GridData.IsMovable(pos + dir.ToVector2Int()))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
