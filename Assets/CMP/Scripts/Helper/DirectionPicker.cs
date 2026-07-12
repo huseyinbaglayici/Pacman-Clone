0a3699d02001e874a12b32fef3c68bd085b90efd
huseyinbaglayici <huseyinow35@hotmail.com>
2026-07-12 18:03:19 +0300

    refactor: extract direction picking from ScatterState

---
+++ Assets/CMP/Scripts/Helper/DirectionPicker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CMP.Scripts.Helper
+{
+    public static class DirectionPicker
+    {
+        public static Direction PickRandom(Vector2Int pos, Direction heading, Func<Vector2Int, bool> isWalkable)
+        {
+            Direction reverse = heading.Reverse();
+            var candidates = new List<Direction>();
+
+            foreach (var dir in GameSettings.DirectionsToCheck)
+            {
+                if (dir == reverse)
+                    continue;
+                if (isWalkable(pos + dir.ToVector2Int()))
+                    candidates.Add(dir);
+            }
+
+            if (candidates.Count > 0)
+                return candidates[Random.Range(0, candidates.Count)];
+
+            return isWalkable(pos + reverse.ToVector2Int()) ? reverse : Direction.None;
+        }
+    }
+}
---
+++ Assets/CMP/Tests/EditMode/DirectionPickerTests.cs
@@ -0,0 +1,53 @@
+﻿using CMP.Scripts;
+using CMP.Scripts.Helper;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace CMP.Tests.EditMode
+{
+    public class DirectionPickerTests
+    {
+        private static bool AllOpen(Vector2Int c) => true;
+
+        [Test]
+        public void PickRandom_NeverReturnsReverse_WhenAlternativesExist()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var chosen = DirectionPicker.PickRandom(Vector2Int.zero, Direction.Right, AllOpen);
+
+                Assert.AreNotEqual(Direction.Left, chosen);
+            }
+        }
+
+        [Test]
+        public void PickRandom_DeadEnd_ReturnsReverse()
+        {
+            bool Walkable(Vector2Int c) => c == Vector2Int.left;
+            var chosen = DirectionPicker.PickRandom(Vector2Int.zero, Direction.Right, Walkable);
+
+            Assert.AreEqual(Direction.Left, chosen);
+        }
+
+        [Test]
+        public void PickRandom_FullyBlocked_ReturnsNone()
+        {
+            var chosen = DirectionPicker.PickRandom(Vector2Int.zero, Direction.Right, _ => false);
+
+            Assert.AreEqual(Direction.None, chosen);
+        }
+
+        [Test]
+        public void PickRandom_OnlyReturnsWalkableDirections()
+        {
+            // sadece yukari ve asagi acik
+            bool Walkable(Vector2Int c) => c == Vector2Int.up || c == Vector2Int.down;
+            for (int i = 0; i < 100; i++)
+            {
+                var chosen = DirectionPicker.PickRandom(Vector2Int.zero, Direction.Right, Walkable);
+
+                Assert.IsTrue(chosen == Direction.Up || chosen == Direction.Down, $"Kapali yon secildi: {chosen}");
+            }
+        }
+    }
+}
